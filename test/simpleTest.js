637cc6d51f64d22f7d3fa0a1e37e451670759c67
Momchil Chernev <mchernev95@gmail.com>
2017-03-17 01:13:31 +0200

    Added tests

--- test/simpleTest.js
+++ test/simpleTest.js
@@ -1,7 +1,29 @@
 var expect = require('chai').expect;
 
+require('chai').should();
+const util = require('../server/utils/utils');
+
 describe('This test', function() {
-    it('should work', function(){
+    it('is expected to work', function() {
         expect(true).to.be.true;
+    });
+});
+
+describe('randomizer', function() {
+    it('should be a function', function() {
+        util.randomizer.should.be.a('function');
+    });
+});
+
+describe('randomizer', function() {
+    it('should return a string', function() {
+        util.randomizer().should.be.a('string');
+    });
+
+});
+
+describe('downloadURI', function() {
+    it('should be a function', function() {
+        util.downloadURI.should.be.a('function');
     });
 });
